d52748ea8ae83e7d08731b7e5a042b43456e56b1
nextwaveaisuite <nextwaveaisuite@gmail.com>
2025-07-21 14:52:23 +1000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '40px' }}>
-      <h1>✅ Welcome to NextForge AI</h1>
-      <p>This is your fully functional SaaS builder starter page.</p>
-    </div>
-  );
-}
